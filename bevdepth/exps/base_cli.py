0829799f5d7404d9e5058d51958efa5ef7659140
= <=>
2024-11-20 18:42:02 +0800

    add comments to base_cli.py to improve code readability and maintainability

--- bevdepth/exps/base_cli.py
+++ bevdepth/exps/base_cli.py
@@ -10,48 +10,57 @@ from bevdepth.utils.torch_dist import all_gather_object, synchronize
 from .nuscenes.base_exp import BEVDepthLightningModel
 
 
-def run_cli(model_class=BEVDepthLightningModel,
-            exp_name='base_exp',
-            use_ema=False,
-            extra_trainer_config_args={}):
+def run_cli(
+    model_class=BEVDepthLightningModel,
+    exp_name="base_exp",
+    use_ema=False,
+    extra_trainer_config_args={},
+):
     parent_parser = ArgumentParser(add_help=False)
+    # 这里使用了pytorch_lightning的add_argparse_args方法，可以直接添加一些训练参数
     parent_parser = pl.Trainer.add_argparse_args(parent_parser)
-    parent_parser.add_argument('-e',
-                               '--evaluate',
-                               dest='evaluate',
-                               action='store_true',
-                               help='evaluate model on validation set')
-    parent_parser.add_argument('-p',
-                               '--predict',
-                               dest='predict',
-                               action='store_true',
-                               help='predict model on testing set')
-    parent_parser.add_argument('-b', '--batch_size_per_device', type=int)
-    parent_parser.add_argument('--seed',
-                               type=int,
-                               default=0,
-                               help='seed for initializing training.')
-    parent_parser.add_argument('--ckpt_path', type=str)
+    parent_parser.add_argument(
+        "-e",
+        "--evaluate",
+        dest="evaluate",
+        action="store_true",
+        help="evaluate model on validation set",
+    )
+    parent_parser.add_argument(
+        "-p",
+        "--predict",
+        dest="predict",
+        action="store_true",
+        help="predict model on testing set",
+    )
+    parent_parser.add_argument("-b", "--batch_size_per_device", type=int)
+    parent_parser.add_argument(
+        "--seed", type=int, default=0, help="seed for initializing training."
+    )
+    parent_parser.add_argument("--ckpt_path", type=str)
     parser = BEVDepthLightningModel.add_model_specific_args(parent_parser)
-    parser.set_defaults(profiler='simple',
-                        deterministic=False,
-                        max_epochs=extra_trainer_config_args.get('epochs', 24),
-                        accelerator='ddp',
-                        num_sanity_val_steps=0,
-                        gradient_clip_val=5,
-                        limit_val_batches=0,
-                        enable_checkpointing=True,
-                        precision=16,
-                        default_root_dir=os.path.join('./outputs/', exp_name))
+    parser.set_defaults(
+        profiler="simple",
+        deterministic=False,
+        max_epochs=extra_trainer_config_args.get("epochs", 24),
+        accelerator="ddp",
+        num_sanity_val_steps=0,
+        gradient_clip_val=5,
+        limit_val_batches=0,
+        enable_checkpointing=True,
+        precision=16,
+        default_root_dir=os.path.join("./outputs/", exp_name),
+    )
     args = parser.parse_args()
     if args.seed is not None:
         pl.seed_everything(args.seed)
-
+    # 调用模型类的初始化方法，传入给定的超参数
     model = model_class(**vars(args))
     if use_ema:
         train_dataloader = model.train_dataloader()
-        ema_callback = EMACallback(
-            len(train_dataloader.dataset) * args.max_epochs)
+        ema_callback = EMACallback(len(train_dataloader.dataset) * args.max_epochs)
+        # 从参数中获取trainer的参数，同时添加回调函数
+        # trainer是pytorch_lightning的训练器，可以自动调用模型的训练和验证方法
         trainer = pl.Trainer.from_argparse_args(args, callbacks=[ema_callback])
     else:
         trainer = pl.Trainer.from_argparse_args(args)
@@ -68,11 +77,13 @@ def run_cli(model_class=BEVDepthLightningModel,
         synchronize()
         len_dataset = len(model.test_dataloader().dataset)
         all_pred_results = sum(
-            map(list, zip(*all_gather_object(all_pred_results))),
-            [])[:len_dataset]
-        all_img_metas = sum(map(list, zip(*all_gather_object(all_img_metas))),
-                            [])[:len_dataset]
-        model.evaluator._format_bbox(all_pred_results, all_img_metas,
-                                     os.path.dirname(args.ckpt_path))
+            map(list, zip(*all_gather_object(all_pred_results))), []
+        )[:len_dataset]
+        all_img_metas = sum(map(list, zip(*all_gather_object(all_img_metas))), [])[
+            :len_dataset
+        ]
+        model.evaluator._format_bbox(
+            all_pred_results, all_img_metas, os.path.dirname(args.ckpt_path)
+        )
     else:
         trainer.fit(model)
